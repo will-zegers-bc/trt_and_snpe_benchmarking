ec9e8875a44e289d669123da95dc2e738b01d7ee
Will Zegers <zegers@braincorporation.com>
2019-09-24 19:04:51 -0700

    Cleaning up data processing script

--- scripts/compile_benchmark_data.py
+++ scripts/compile_benchmark_data.py
@@ -1,9 +1,7 @@
 #! /usr/bin/env python3
 
 from collections import Iterable
-import logging
 import os
-import sys
 
 try:
     import matplotlib.pyplot as plt
@@ -11,8 +9,6 @@ except ImportError as ex:
     print("[-] matplotlib not found. Plotting functionality diasbled")
 import numpy as np
 import pandas as pd
-
-from model_meta import NETS
 
 KB_TO_MB = 1. / 1024
 
@@ -22,21 +18,9 @@ MEMORY_DIR = 'data/memory'
 OUTPUT_CSV = 'data/benchmarks.csv'
 
 DATA_TYPES = ['tf', 'trt-float', 'trt-half']
-COLUMN_NAMES = ['net_name', 'data_type', 'throughput', 'peak_uss', 'peak_pss',
-                'peak_rss', 'accuracy', 'precision', 'recall']
-
-
-def _make_record_dict(net_name, data_type, df):
-    return {
-        'net_name': net_name,
-        'data_type': data_type,
-        'uss_avg': int(KB_TO_MB * df.uss.mean()),
-        'uss_max': int(KB_TO_MB * df.uss.max()),
-        'pss_avg': int(KB_TO_MB * df.pss.mean()),
-        'pss_max': int(KB_TO_MB * df.pss.max()),
-        'rss_avg': int(KB_TO_MB * df.rss.mean()),
-        'rss_max': int(KB_TO_MB * df.rss.max()),
-    }
+COLUMN_NAMES = ['net_name', 'data_type', 'throughput',
+                'peak_uss', 'peak_pss', 'peak_rss',
+                'accuracy', 'precision', 'recall']
 
 
 def build_dataframe(mem_dir=MEMORY_DIR, lat_dir=LATENCY_DIR, columns=COLUMN_NAMES, acc_dir=ACCURACY_DIR):
@@ -46,11 +30,9 @@ def build_dataframe(mem_dir=MEMORY_DIR, lat_dir=LATENCY_DIR, columns=COLUMN_NAME
         mem_df = pd.read_csv(os.path.join(mem_dir, data_type + '.csv'))
         lat_df = pd.read_csv(os.path.join(lat_dir, data_type + '.csv'))
         acc_df = pd.read_csv(os.path.join(acc_dir, data_type + '.csv'))
-        for net_name, net_meta in NETS.items():
-            if 'exclude' in net_meta.keys() and net_meta['exclude'] is True:
-                logging.info("Skipping {}".format(net_name))
-                continue
-    
+
+        assert set(mem_df.net_name.unique()) == set(lat_df.net_name.unique()) == set(acc_df.net_name.unique())
+        for net_name in mem_df.net_name.unique():
             rec = {
                 'net_name': net_name,
                 'data_type': data_type,
@@ -70,10 +52,10 @@ def build_dataframe(mem_dir=MEMORY_DIR, lat_dir=LATENCY_DIR, columns=COLUMN_NAME
     return pd.DataFrame(records, columns=columns)
 
 
-def plot_common(df, column_name, config):
-    fig, axs = plt.subplots(figsize=(24, 13.5), nrows=3, ncols=5)
+def plot(df, column_name, config):
+    fig, axs = plt.subplots(figsize=(24, 13.5), nrows=3, ncols=6)
     fig.tight_layout()
-    fig.subplots_adjust(left=0.05, top=0.925, bottom=0.025, hspace=0.15, wspace=0.25)
+    fig.subplots_adjust(left=0.05, top=0.925, bottom=0.025, hspace=0.20, wspace=0.35)
     fig.suptitle(config['title'], fontsize=16)
 
     global_max = df[column_name].max() * 1.1
@@ -88,6 +70,8 @@ def plot_common(df, column_name, config):
             for i, (color, xgroup) in enumerate(zip(config['plot']['color'], config['xgroups'])):
                 subgroup = group.loc[group['data_type'] == xgroup]
                 ax.bar(ind+(i*0.25), subgroup[column_name].values[0], label=xgroup, width=0.25, color=color)
+        else:
+            group.plot(kind='bar', x='data_type', y=column_name, ax=ax, **config['plot'])
         
         plt.setp(ax.xaxis.get_majorticklabels(), rotation=0)
         ax.set_title(key)
@@ -110,122 +94,27 @@ def plot_common(df, column_name, config):
     plt.show()
 
 
-def plot_grouped(df, column_name, config):
-    fig, axs = plt.subplots(figsize=(24, 13.5), nrows=3, ncols=5)
-    fig.tight_layout()
-    fig.subplots_adjust(left=0.05, top=0.925, bottom=0.025, hspace=0.15, wspace=0.25)
-    fig.suptitle(config['title'], fontsize=16)
-
-    global_max = df[column_name].max() * 1.1
-    if isinstance(global_max, Iterable):                                         
-        global_max = max(global_max)                                              
-
-    grouped = df.groupby('net_name')
-    for key, ax in zip(sorted(grouped.groups.keys()), axs.flatten()):
-        group = grouped.get_group(key)
-        ind = np.arange(3)
-        for i, (color, xgroup) in enumerate(zip(config['plot']['color'], config['xgroups'])):
-            subgroup = group.loc[group['data_type'] == xgroup]
-            ax.bar(ind+(i*0.25), subgroup[column_name].values[0], label=xgroup, width=0.25, color=color)
-        
-        ax.set_xticklabels(column_name)
-        plt.setp(ax.xaxis.get_majorticklabels(), rotation=0)
-        ax.set_xticks(ind + 0.25 / 2)
-        ax.set_title(key)
-        ax.set_xlabel('')
-        ax.set_ylabel(config['axis_name'])
-        ax.set_yscale(**config['scale'])
-        if config['ylims'] == 'global':
-            limits = {'top': global_max}
-        elif config['ylims'] == 'local_range':
-            local_max = group[column_name].max() * 1.02
-            local_min = group[column_name].min() * 0.98
-            if isinstance(local_max, Iterable):                                         
-                local_max = max(local_max)                                              
-                local_min = min(local_min)                                              
-            limits = {'top': local_max, 'bottom': local_min}
-        else:
-            limits = {}
-        ax.set_ylim(**limits)                                          
-
-        if not config['plot'].get('label', ''):                                 
-            ax.legend().remove()                                                
-
-    plt.show()
-    
-
-def plot(df, column_name, config):
-    fig, axs = plt.subplots(figsize=(24, 13.5), nrows=3, ncols=5)               
-    fig.tight_layout()                                                          
-    fig.subplots_adjust(left=0.05, top=0.925, bottom=0.025, hspace=0.15, wspace=0.25)
-    fig.suptitle(config['title'], fontsize=16)                                  
-                                                                                
-    global_max = df[column_name].max() * 1.1
-    if isinstance(global_max, Iterable):                                         
-        global_max = max(global_max)                                              
-                                                                                
-    grouped = df.groupby('net_name')                                            
-    for key, ax in zip(sorted(grouped.groups.keys()), axs.flatten()):           
-        group = grouped.get_group(key)                                          
-        group.plot(kind='bar', x='data_type', y=column_name, ax=ax, **config['plot'])
-                                                                                
-        plt.setp(ax.xaxis.get_majorticklabels(), rotation=0)                    
-        ax.set_title(key)                                                       
-        ax.set_xlabel('')                                                       
-                                                                                
-        ax.set_ylabel(config['axis_name'])                                      
-        ax.set_yscale(**config['scale'])                                        
-        if config['ylims'] == 'global':
-            limits = {'top': global_max}
-        elif config['ylims'] == 'local_range':
-            local_max = group[column_name].max() * 1.02
-            local_min = group[column_name].min() * 0.98
-            if isinstance(local_max, Iterable):                                         
-                local_max = max(local_max)                                              
-                local_min = min(local_min)                                              
-            limits = {'top': local_max, 'bottom': local_min}
-        else:
-            limits = {}
-        ax.set_ylim(**limits)                                          
-                                                                                
-        if not config['plot'].get('label', ''):                                 
-            ax.legend().remove()                                                
-                                                                                
-    plt.show()                                                                  
-
-
 if __name__ == '__main__':
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
     if not os.path.isfile(OUTPUT_CSV):
-        logger.info("Creating benchmark dataframe")
         df = build_dataframe()
-        df.to_csv(OUTPUT_CSV)
+        df.to_csv(OUTPUT_CSV, index=False)
     else:
-        logger.info("Using existing benchmark dataframe")
         df = pd.read_csv(OUTPUT_CSV)
 
+    color = ('tab:red', 'tab:green', 'tab:blue')
     # Plot max memory usage
-    metrics = ['uss_max', 'pss_max', 'rss_max']
+    metrics = ['peak_uss', 'peak_pss', 'peak_rss']
     plt_config = {
         'axis_name': 'Memory Footprint (MB)',
         'scale': {'value': 'linear'},
         'title': 'Maximum Memory Footprint',
-        'xgroups': ('tf', 'float', 'half'),
+        'xgroups': ('tf', 'trt-float', 'trt-half'),
         'plot': {
-            'color': ('tab:blue', 'tab:red', 'tab:green'),
+            'color': color,
         },
         'ylims': 'global',
     }
-    plot_grouped(df, metrics, plt_config)
-
-    # Plot avg memory usage
-    metrics = ['uss_avg', 'pss_avg', 'rss_avg']
-    plt_config.update({
-        'axis_name': 'Average Memory Footprint (MB)',
-        'title': 'Average Memory Footprint',
-    })
-    plot_grouped(df, metrics, plt_config)
+    plot(df, metrics, plt_config)
 
    # Plot throughput
     plt_config = {
@@ -234,34 +123,22 @@ if __name__ == '__main__':
         'title': 'Throughput (Inferences per Second)',
         'ylims': 'default',
         'plot': {
-            'color': 'tab:purple',
+            'color': color[0],
         }
     }
     plot(df, 'throughput', plt_config)
 
-    # Plot performance multiplier
-    plt_config = {
-        'axis_name': 'Performance Gain',
-        'scale': {'value': 'log', 'basey': 2},
-        'title': 'Performance gain',
-        'ylims': 'global',
-        'plot': {
-            'color': 'tab:orange',
-        }
-    }
-    plot(df, 'gain', plt_config)
-   
    # Plot accuracy metrics
     metrics = ['accuracy', 'precision', 'recall']
     plt_config = {
         'axis_name': 'Proportion correct',
         'scale': {'value': 'linear'},
         'title': 'Accuracy Metrics',
-        'xgroups': ('tf', 'float', 'half'),
+        'xgroups': ('tf', 'trt-float', 'trt-half'),
         'ylims': 'local_range',
         'plot': {
-            'color': ('tab:brown', 'tab:gray', 'tab:olive'),
+            'color': color,
             'width': 0.15
         },
     }
-    plot_grouped(df, metrics, plt_config)
+    plot(df, metrics, plt_config)
