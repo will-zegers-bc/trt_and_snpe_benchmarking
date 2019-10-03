6528fa2cfad289010ce332389aaafc175237fa76
Will Zegers <zegers@braincorporation.com>
2019-10-03 10:16:01 -0700

    Updated data aggr. to use SNPE results

--- scripts/compile_benchmark_data.py
+++ scripts/compile_benchmark_data.py
@@ -16,37 +16,45 @@ KB_TO_MB = 1. / 1024
 ACCURACY_DIR = 'data/accuracy'
 THROUGHPUT_DIR = 'data/throughput'
 MEMORY_DIR = 'data/memory'
-OUTPUT_CSV = 'data/benchmarks.csv'
+OUTPUT_CSV = 'data/benchmarks-{}.csv'
 
-DATA_TYPES = ['tf', 'float', 'half']
-COLUMN_NAMES = ['net_name', 'data_type', 'throughput',
-                'peak_uss', 'peak_pss', 'peak_rss',
-                'accuracy', 'precision', 'recall']
+RUNTIMES = {
+    'nv_tx2': ('tf', 'float', 'half'),
+    'qc_sd845': ('cpu', 'gpu', 'g16', 'dsp'),
+}
+COLUMN_NAMES = ['net_name', 'runtime', 'throughput', 'peak_uss', 'peak_pss',
+                'peak_rss', 'accuracy', 'precision', 'recall', 'platform']
 
 
 def build_dataframe(platform, mem_dir=MEMORY_DIR, tp_dir=THROUGHPUT_DIR, acc_dir=ACCURACY_DIR, columns=COLUMN_NAMES):
     records = []
 
-    for data_type in DATA_TYPES:
-        mem_df = pd.read_csv(os.path.join(mem_dir, platform, data_type + '.csv'))
-        tp_df = pd.read_csv(os.path.join(tp_dir, platform, data_type + '.csv'))
-        acc_df = pd.read_csv(os.path.join(acc_dir, platform, data_type + '.csv'))
+    for runtime in RUNTIMES[platform]:
+        mem_df = pd.read_csv(os.path.join(mem_dir, platform, runtime + '.csv'))
+        tp_df = pd.read_csv(os.path.join(tp_dir, platform, runtime + '.csv'))
+        acc_df = pd.read_csv(os.path.join(acc_dir, platform, runtime + '.csv'))
 
+        print(runtime)
+        print(set(mem_df.net_name.unique()) == set(tp_df.net_name.unique()))
+        print(set(mem_df.net_name.unique()) == set(acc_df.net_name.unique()))
+        print(set(tp_df.net_name.unique()) == set(acc_df.net_name.unique()))
         assert set(mem_df.net_name.unique()) == set(tp_df.net_name.unique()) == set(acc_df.net_name.unique())
         for net_name in mem_df.net_name.unique():
             rec = {
                 'net_name': net_name,
-                'data_type': data_type,
+                'runtime': runtime,
 
                 'throughput': tp_df.loc[tp_df['net_name'] == net_name, 'throughput'].values[0],
 
-                'peak_uss': int(KB_TO_MB * mem_df.loc[mem_df['net_name'] == net_name, 'peak_uss'].values[0]),
-                'peak_pss': int(KB_TO_MB * mem_df.loc[mem_df['net_name'] == net_name, 'peak_pss'].values[0]),
-                'peak_rss': int(KB_TO_MB * mem_df.loc[mem_df['net_name'] == net_name, 'peak_rss'].values[0]),
+                'peak_uss': KB_TO_MB * mem_df.loc[mem_df['net_name'] == net_name, 'peak_uss'].values[0],
+                'peak_pss': KB_TO_MB * mem_df.loc[mem_df['net_name'] == net_name, 'peak_pss'].values[0],
+                'peak_rss': KB_TO_MB * mem_df.loc[mem_df['net_name'] == net_name, 'peak_rss'].values[0],
 
                 'accuracy': acc_df.loc[acc_df['net_name'] == net_name, 'accuracy'].values[0],
                 'precision': acc_df.loc[acc_df['net_name'] == net_name, 'precision'].values[0],
                 'recall': acc_df.loc[acc_df['net_name'] == net_name, 'recall'].values[0],
+
+                'platform': platform,
             }
             records.append(rec)
 
@@ -70,7 +78,7 @@ def plot(df, column_name, config):
             ind = np.arange(3)
             for i, (color, xgroup) in enumerate(zip(config['plot']['color'], config['xgroups'])):
                 subgroup = group.loc[group['data_type'] == xgroup]
-                ax.bar(ind+(i*0.25), subgroup[column_name].values[0],  width=0.25, color=color)
+                ax.bar(ind+(i*0.25), subgroup[column_name].values[0],  width=0.25, color=color, label='blue')
         else:
             group.plot(kind='bar', x='data_type', y=column_name, ax=ax, **config['plot'])
             ax.legend().remove()
@@ -98,50 +106,52 @@ if __name__ == '__main__':
     parser.add_argument('platform', type=str, choices=['nv_tx2', 'qc_sd845'])
     args = parser.parse_args()
 
-    if not os.path.isfile(OUTPUT_CSV):
-        df = build_dataframe()
-        df.to_csv(OUTPUT_CSV, index=False)
+    output_csv = OUTPUT_CSV.format(args.platform)
+    if not os.path.isfile(output_csv):
+        df = build_dataframe(args.platform)
+        df.to_csv(output_csv, index=False)
     else:
-        df = pd.read_csv(OUTPUT_CSV)
+        df = pd.read_csv(output_csv)
 
-    color = ('tab:red', 'tab:green', 'tab:blue')
+    print(df)
+#    color = ('tab:red', 'tab:green', 'tab:blue')
     # Plot max memory usage
-    metrics = ['peak_uss', 'peak_pss', 'peak_rss']
-    plt_config = {
-        'axis_name': 'Memory Footprint (MB)',
-        'scale': {'value': 'linear'},
-        'title': 'Peak Memory Usage',
-        'xgroups': ('tf', 'trt-float', 'trt-half'),
-        'plot': {
-            'color': color,
-        },
-        'ylims': 'global',
-    }
-    plot(df, metrics, plt_config)
-
-    # Plot throughput
-    plt_config = {
-        'axis_name': 'Throughput (infc/s)',
-        'scale': {'value': 'linear'},
-        'title': 'Throughput (Inferences per Second)',
-        'ylims': 'default',
-        'plot': {
-            'color': color[0],
-        }
-    }
-    plot(df, 'throughput', plt_config)
-
-    # Plot accuracy metrics
-    metrics = ['accuracy', 'precision', 'recall']
-    plt_config = {
-        'axis_name': 'Score',
-        'scale': {'value': 'linear'},
-        'title': 'Accuracy Metrics',
-        'xgroups': ('tf', 'trt-float', 'trt-half'),
-        'ylims': 'local_range',
-        'plot': {
-            'color': color,
-            'width': 0.15
-        },
-    }
-    plot(df, metrics, plt_config)
+#    metrics = ['peak_uss', 'peak_pss', 'peak_rss']
+#    plt_config = {
+#        'axis_name': 'Memory Footprint (MB)',
+#        'scale': {'value': 'linear'},
+#        'title': 'Peak Memory Usage',
+#        'xgroups': ('tf', 'trt-float', 'trt-half'),
+#        'plot': {
+#            'color': color,
+#        },
+#        'ylims': 'global',
+#    }
+#    plot(df, metrics, plt_config)
+#
+#    # Plot throughput
+#    plt_config = {
+#        'axis_name': 'Throughput (infc/s)',
+#        'scale': {'value': 'linear'},
+#        'title': 'Throughput (Inferences per Second)',
+#        'ylims': 'default',
+#        'plot': {
+#            'color': color[0],
+#        }
+#    }
+#    plot(df, 'throughput', plt_config)
+#
+#    # Plot accuracy metrics
+#    metrics = ['accuracy', 'precision', 'recall']
+#    plt_config = {
+#        'axis_name': 'Score',
+#        'scale': {'value': 'linear'},
+#        'title': 'Accuracy Metrics',
+#        'xgroups': ('tf', 'trt-float', 'trt-half'),
+#        'ylims': 'local_range',
+#        'plot': {
+#            'color': color,
+#            'width': 0.15
+#        },
+#    }
+#    plot(df, metrics, plt_config)
